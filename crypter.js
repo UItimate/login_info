9f1e30cc826ac6ec41a7d588474fac1185f53b07
UItimate <100000087+UItimate@users.noreply.github.com>
2022-09-22 10:41:49 -0400

    Add files via upload

---
+++ crypter.js
@@ -0,0 +1,25 @@
+const crypt = (salt, text) => {
+  const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+  const byteHex = (n) => ("0" + Number(n).toString(16)).substr(-2);
+  const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
+
+  return text
+    .split("")
+    .map(textToChars)
+    .map(applySaltToChar)
+    .map(byteHex)
+    .join("");
+};
+
+const decrypt = (salt, encoded) => {
+  const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+  const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
+  return encoded
+    .match(/.{1,2}/g)
+    .map((hex) => parseInt(hex, 16))
+    .map(applySaltToChar)
+    .map((charCode) => String.fromCharCode(charCode))
+    .join("");
+};
+const users = ["6b6e676364", "44657e55606b696568"];
+const passes = ["3a33383b383a3a3d", "3b38393e3f3e39383b"];
